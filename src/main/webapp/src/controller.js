3e0c149e357e0b731be62f9df9f542cfef3088b6
tcrow <luowei31@gmail.com>
2017-01-13 18:01:18 +0800

    modify cluster page

--- src/main/webapp/src/controller.js
+++ src/main/webapp/src/controller.js
@@ -16,6 +16,7 @@ app.controller('ClusterController', ['$scope','$location','$http','Notification'
     $scope.clusterMap = {};//cluster:brokerNameList
     $scope.brokerMap = {};//brokerName:{id:addr}
     $scope.brokerDetail = {};//{brokerName,id:detail}
+    $scope.brokerAddrsArray = {};
     $http({
         method: "GET",
         url: "/cluster/list.query"
@@ -23,11 +24,32 @@ app.controller('ClusterController', ['$scope','$location','$http','Notification'
         if (resp.status == 0) {
             $scope.clusterMap = resp.data.clusterInfo.clusterAddrTable;
             $scope.brokerMap = resp.data.clusterInfo.brokerAddrTable;
+            console.info($scope.brokerMap);
             $scope.brokerDetail = resp.data.brokerServer;
+            $scope.clusterList = [];
+            $.each($scope.clusterMap,function(key,brokerList){
+                $scope.clusterList.push(key)
+            })
+            console.info($scope.brokerDetail)
+            $.each($scope.brokerMap,function(key,broker){
+                $.each(broker.brokerAddrs,function(i,brokerAddrs){
+                    var brokerAddrs = {
+                        index:i,
+                        brokerAddrs:brokerAddrs,
+                        brokerName:brokerName
+                    }
+                    brokerAddrsArray.push(brokerAddrs);
+                })
+            })
+            $scope.switchCluster('DefaultCluster');
         }else{
             Notification.error({message: resp.errMsg, delay: 2000});
         }
     });
+
+    $scope.switchCluster = function(clusterName){
+        $scope.brokerNameList = $scope.clusterMap[clusterName];
+    }
 
     $scope.showDetail = function (brokerDetail,brokerName,index) {
         $scope.detail = brokerDetail[brokerName][index];
